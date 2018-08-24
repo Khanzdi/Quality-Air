7da1d0109e7335064f3080a475aa3d962393f885
Kevin <khanzdi@unac.edu.co>
2018-08-23 22:34:16 -0500

    Upload

---
+++ app/src/main/java/co/edu/unac/iotunac/View/MainActivity.java
@@ -0,0 +1,17 @@
+package co.edu.unac.iotunac.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import co.edu.unac.iotunac.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
